be2ef0e918130f92df7c812e963aa4e6b9093049
Yorch <20454656+jorge-alejandro@users.noreply.github.com>
2023-04-27 12:24:18 +0200

    Scoring system and time elapsed in the final message

--- js/cats.js
+++ js/cats.js
@@ -1,4 +1,5 @@
-// declare vairbles for cards, cards flipped, for locking the board , and for identifying both cards clicked
+// Declare variables for cards, cards flipped, for locking the board, and for identifying both cards clicked
+
 const cards = document.querySelectorAll('.memory-card');
 let hasFlippedCard = false;
 let lockBoard = false;
@@ -6,110 +7,196 @@ let firstCard, secondCard;
 let matches = 0;
 let misses = 0;
 let tries = 0;
+
+//Variables for the score sistem and metrics
+
 const matchesSpan = document.getElementById('matches');
 const missesSpan = document.getElementById('misses');
 const triesSpan = document.getElementById('tries');
-let time = 0;
-let timer;
+const finalResultSpan = document.getElementById('playAgainMessage');
 
+// Add listener over the cards to be able to play with them
 
-// we add listener over the cards to be able to play with them
+cards.forEach(card => card.addEventListener('click', flipCard));
 
-cards.forEach(card => card.addEventListener('click',flipCard));
+// This function includes the property flips to the cards so the effect
+// is added using the CSS
 
-// this functions inclludes the property flips to the cards so the effect is added using the CSS
 function flipCard() {
-    if(lockBoard) return;
-    //if(this === firstCard) return;
-    this.classList.add('flip');
-
-    if (!hasFlippedCard) {
-        hasFlippedCard = true;
-        firstCard = this;
-    } else {
-        hasFlippedCard = false;
-        secondCard = this;
-        
-        checkMatch();
-
-    };
+  if (lockBoard) return;
+  this.classList.add('flip');
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+  } else {
+    hasFlippedCard = false;
+    secondCard = this;
+    checkMatch();
+  }
 };
 
-//This function is meant to check if both card selected by the users has the same image. If both cards are the same the listener are erased so both card remain flipped. If not both card return to originalposition.
+// This function is meant to check if both cards selected by the users have the same image.
+// If both cards are the same, the listener is erased so both cards remain flipped. 
+//If not, both cards return to the original position.
+//It also counts the tries, misses and matches and capture them to show them in the web
+
 const checkMatch = () => {
-    if (firstCard.dataset.framework === secondCard.dataset.framework) {
-        firstCard.removeEventListener('click', flipCard);
-        secondCard.removeEventListener('click', flipCard);
-        matches += 1;
-        matchesSpan.textContent = matches;
-        tries += 1;
-        triesSpan.textContent = tries;
-        finishGame();
-    } else {
-        lockBoard = true;
-        setTimeout(() => {
-            firstCard.classList.remove('flip');
-            secondCard.classList.remove('flip');
-            lockBoard = false;
-            misses += 1;
-            missesSpan.textContent = misses;
-            tries += 1;
-            triesSpan.textContent = tries;
-        }, 1500);
-    }
+  if (firstCard.dataset.framework === secondCard.dataset.framework) {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    matches += 1;
+    matchesSpan.textContent = matches;
+    tries += 1;
+    triesSpan.textContent = tries;
+    finishGame();
+  } else {
+    lockBoard = true;
+    setTimeout(() => {
+      firstCard.classList.remove('flip');
+      secondCard.classList.remove('flip');
+      lockBoard = false;
+      misses += 1;
+      missesSpan.textContent = misses;
+      tries += 1;
+      triesSpan.textContent = tries;
+    }, 1500);
+  }
 };
 
-//to make the card appear randmly
+// To make the cards appear randomly
 
 const shuffle = () => {
-
-    cards.forEach(card => {
-        let randomPosition =Math.floor(Math.random() * 12);
-        card.style.order =randomPosition;
-    });
+  cards.forEach(card => {
+    let randomPosition = Math.floor(Math.random() * 12);
+    card.style.order = randomPosition;
+  });
 };
-
-// we call the function to make the 
 
 shuffle();
 
-
-// check if the game is complete
+// Check if the game is complete
 
 const finishGame = () => {
-    if (matches === 6) {
-      console.log(`you win motherfucker`);
-      cards.forEach(card => card.addEventListener('click',flipCard));
+  if (matches === 6) {
+    stopTimer(); 
+    document.getElementById('startBtn').style.display = 'none';
+    document.getElementById('playAgainBtn').style.display = 'block';
+    document.getElementById('playAgainMessage').style.display = 'block';
+    document.getElementById('startMessage').style.display = 'none';
+    overlay.style.display = 'flex';
+    popup.style.display = 'flex';
+    endTime = new Date().getTime();
+    let elapsedTime = calculateElapsedTime();
+    const elapsedTimeFormatted = msToTime(elapsedTime);
+    let finalScore = Math.round(calculateFinalScore(elapsedTime,tries));
+    finalResultSpan.textContent = `Your final result is misses: ${misses}, matches ${matches} y tries ${tries}. And the time is ${elapsedTimeFormatted}. Your final score is ${finalScore}/1000`;
+  }
+};
 
-        // Show the popup
-    const popup = document.getElementById('popup');
-    popup.style.display = 'block';
-  
-      // reset game
-      matches = 0;
-      misses = 0;
-      tries = 0;
-      matchesSpan.textContent = matches;
-      missesSpan.textContent = misses;
-      triesSpan.textContent = tries;
-      cards.forEach(card => {
-        card.classList.remove('flip');
-        card.addEventListener('click',flipCard);
-      });
-      shuffle();
-    };
-  };
-  
-//timer 
+
+//Showing on and off the popup for starting the game
+// Also reseting the game after clicking in play again.
+
+const overlay = document.querySelector(".overlay");
+const popup = document.querySelector(".popup");
+const startBtn = document.querySelector("#startBtn");
+const playAgainBtn = document.getElementById("playAgainBtn");
+
+
+startBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+  popup.style.display = "none";
+  startTimer(); 
+});
+
+playAgainBtn.addEventListener("click", () => {
+  overlay.style.display = "none";
+  popup.style.display = "none"; 
+  matches = 0;
+  misses = 0;
+  tries = 0;
+  startTimer();
+  matchesSpan.textContent = matches;
+  missesSpan.textContent = misses;
+  triesSpan.textContent = tries;
+
+  cards.forEach
+  (card => {
+    card.classList.remove('flip');
+    card.addEventListener('click', flipCard);
+  });
+  shuffle();
+});
+
+overlay.style.display = "flex";
+popup.style.display = "flex";
+
+// Add timer variables
+
+let startTime;
+let timerInterval;
+const timerElement = document.getElementById("timer");
+let endTime;
+let time = 0;
+let timer;
+let elapsedTime = 0;
+
+// Update the timer
+
+function updateTimer() {
+  const currentTime = new Date().getTime();
+  const elapsedTime = currentTime - startTime;
+  const minutes = Math.floor(elapsedTime / 60000);
+  const seconds = Math.floor((elapsedTime % 60000) / 1000);
+  timerElement.textContent = `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+};
+
+// Start the timer
 
 function startTimer() {
-  timer = setInterval(() => {
-    time++;
-    console.log(time);
-  }, 1000);
-}
+  startTime = new Date().getTime();
+  timerInterval = setInterval(updateTimer, 1000);
+};
+
+// Stop the timer
 
 function stopTimer() {
-  clearInterval(timer);
+  clearInterval(timerInterval);
+};
+
+//calculate the elapsed time
+
+function calculateElapsedTime() {
+  const endTime = new Date().getTime();
+  return endTime - startTime;
 }
 
+
+// Misilecons translator to minutes and seconds to show it in the finnal mesage
+
+function msToTime(duration) {
+  const milliseconds = parseInt((duration % 1000) / 100),
+        seconds = parseInt((duration / 1000) % 60),
+        minutes = parseInt((duration / (1000 * 60)) % 60);
+
+  return `${minutes}m ${seconds}s`;
+};
+
+//Socring system
+
+const maxTime = 60000;
+const maxTries = 12;
+const minTries = 6;
+const weightTime = 0.5;
+const weightTries = 0.5;
+
+
+function calculateFinalScore(elapsedTime, tries) {
+  let timeFactor = (maxTime - elapsedTime) / maxTime;
+  let triesFactor = 1 - ((tries - minTries) / (maxTries - minTries));
+  let finalScore = 1000 * (timeFactor * weightTime + triesFactor * weightTries);
+
+  return finalScore;
+}
+
+//more things
